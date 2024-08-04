3abe762e9f2017aaccad19ed725fc3c7ba4e2e0d
Lucas Pinheiro <pinheiro.lucash@gmail.com>
2024-08-04 14:53:29 -0300

    lading page primeira versão

---
+++ src/scripts/main.js
@@ -0,0 +1,25 @@
+AOS.init();
+
+const partyDate = new Date("Oct 10, 2024 20:00");
+const partyTimeStamp = partyDate.getTime();
+
+const countTime = setInterval(function() {
+    const now = new Date();
+    const nowStamp = now.getTime();
+    const distaceToparty = partyTimeStamp - nowStamp;
+    
+
+    const daysToParty = Math.floor(distaceToparty / (1000 * 60 * 60 * 24));
+    const hoursToParty = Math.floor((distaceToparty % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutesToParty = Math.floor((distaceToparty % (1000 * 60 * 60)) / (1000 * 60));
+    const secondsToParty = Math.floor((distaceToparty % (1000 * 60)) / 1000);
+   
+    document.getElementById('contador').innerHTML = `${daysToParty}d ${hoursToParty}h ${minutesToParty}m ${secondsToParty}s`;
+
+    if (distaceToparty < 0) {
+        clearInterval(countTime);
+        document.getElementById('contador').innerHTML = `Espere a próxima!`;
+    }
+
+}, 1000)
+
